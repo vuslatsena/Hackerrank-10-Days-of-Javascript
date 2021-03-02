72b15c192f047d3ec779a384b8f2e3dbe4b57ca2
VuslatSena <vuslatsena23@gmail.com>
2021-03-02 10:51:14 +0300

    day3

---
+++ Day3/Day3-Arrays.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let uniqueNums = Array.from(new Set(nums));
+    let sortedNums = uniqueNums.sort((a, b) => b - a);
+    return sortedNums[1];
+}
+
